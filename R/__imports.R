fb606290c69711a97df917c6c01feb71ac06ca92
oberstal <joberstaller@gmail.com>
2021-05-06 13:06:33 -0400

    added imported functions

---
+++ R/__imports.R
@@ -0,0 +1,89 @@
+
+#' re-export magrittr pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' import topGO functions to auto-load topGO with pack.test
+#'
+#' @name runTest
+#' @rdname runTest
+#' @keywords internal
+#' @export
+#' @importFrom topGO runTest
+NULL
+
+#' import topGO functions to auto-load topGO with pack.test
+#'
+#' @name sigGenes
+#' @rdname sigGenes
+#' @keywords internal
+#' @export
+#' @importFrom topGO sigGenes
+NULL
+
+
+#' import topGO functions to auto-load topGO with pack.test
+#'
+#' @name printGraph
+#' @rdname printGraph
+#' @keywords internal
+#' @export
+#' @importFrom topGO printGraph
+NULL
+
+#' import topGO functions to auto-load topGO with pack.test
+#'
+#' @name GenTable
+#' @rdname GenTable
+#' @keywords internal
+#' @export
+#' @importFrom topGO GenTable
+NULL
+
+
+#' import topGO functions to auto-load topGO with pack.test
+#'
+#' @name genesInTerm
+#' @rdname genesInTerm
+#' @keywords internal
+#' @export
+#' @importFrom topGO genesInTerm
+NULL
+
+
+#' import topGO functions to auto-load topGO with pack.test
+#'
+#' @name readMappings
+#' @rdname readMappings
+#' @keywords internal
+#' @export
+#' @importFrom topGO readMappings
+NULL
+
+
+#' import plyr function to auto-load plyr with pack.test
+#'
+#' @name ldply
+#' @rdname ldply
+#' @keywords internal
+#' @export
+#' @importFrom plyr ldply
+NULL
+
+
+#' import plyr function to auto-load plyr with pack.test
+#'
+#' @name bind_rows
+#' @rdname bind_rows
+#' @keywords internal
+#' @export
+#' @importFrom plyr bind_rows
+NULL
+
+
