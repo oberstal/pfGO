665a31192facd4ca98d4183d49833d61495b3a7e
oberstal <joberstaller@gmail.com>
2021-11-12 16:51:01 -0500

    import full topGO package

--- R/__imports.R
+++ R/__imports.R
@@ -4,7 +4,7 @@
 #' @name %>%
 #' @rdname pipe
 #' @keywords internal
-#' @export
+# #' @export
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
@@ -14,76 +14,87 @@ NULL
 #' @name bind_rows
 #' @rdname bind_rows
 #' @keywords internal
-#' @export
+# #' @export
 #' @importFrom dplyr bind_rows
 NULL
-
-#' runTest (imported from topGO)
-#'
-#' @name runTest
-#' @rdname runTest
-#' @keywords internal
-#' @export
-#' @importFrom topGO runTest
-NULL
-
-#' sigGenes (imported from topGO)
-#'
-#' @name sigGenes
-#' @rdname sigGenes
-#' @keywords internal
-#' @export
-#' @importFrom topGO sigGenes
-NULL
-
-
-#' printGraph (imported from topGO)
-#'
-#' @name printGraph
-#' @rdname printGraph
-#' @keywords internal
-#' @export
-#' @importFrom topGO printGraph
-NULL
-
-#' GenTable (imported from topGO)
-#'
-#' @name GenTable
-#' @rdname GenTable
-#' @keywords internal
-#' @export
-#' @importFrom topGO GenTable
-NULL
-
-
-#' genesInTerm (imported from topGO)
-#'
-#' @name genesInTerm
-#' @rdname genesInTerm
-#' @keywords internal
-#' @export
-#' @importFrom topGO genesInTerm
-NULL
-
-
-#' readMappings (imported from topGO)
-#'
-#' @name readMappings
-#' @rdname readMappings
-#' @keywords internal
-#' @export
-#' @importFrom topGO readMappings
-NULL
-
 
 #' ldply (imported from plyr)
 #'
 #' @name ldply
 #' @rdname ldply
 #' @keywords internal
-#' @export
+# #' @export
 #' @importFrom plyr ldply
 NULL
+
+
+#' topGO
+#' @name topGO
+#' @rdname topGO
+#' @keywords internal
+#' @import topGO
+NULL
+
+# comment out topGO individual functions, import full package instead
+# #' runTest (imported from topGO)
+# #'
+# #' @name runTest
+# #' @rdname runTest
+# #' @keywords internal
+# #' @export
+# #' @importFrom topGO runTest
+# NULL
+#
+# #' sigGenes (imported from topGO)
+# #'
+# #' @name sigGenes
+# #' @rdname sigGenes
+# #' @keywords internal
+# #' @export
+# #' @importFrom topGO sigGenes
+# NULL
+#
+#
+# #' printGraph (imported from topGO)
+# #'
+# #' @name printGraph
+# #' @rdname printGraph
+# #' @keywords internal
+# #' @export
+# #' @importFrom topGO printGraph
+# NULL
+#
+# #' GenTable (imported from topGO)
+# #'
+# #' @name GenTable
+# #' @rdname GenTable
+# #' @keywords internal
+# #' @export
+# #' @importFrom topGO GenTable
+# NULL
+#
+#
+# #' genesInTerm (imported from topGO)
+# #'
+# #' @name genesInTerm
+# #' @rdname genesInTerm
+# #' @keywords internal
+# #' @export
+# #' @importFrom topGO genesInTerm
+# NULL
+#
+#
+# #' readMappings (imported from topGO)
+# #'
+# #' @name readMappings
+# #' @rdname readMappings
+# #' @keywords internal
+# #' @export
+# #' @importFrom topGO readMappings
+# NULL
+#
+
+
 
 
 
