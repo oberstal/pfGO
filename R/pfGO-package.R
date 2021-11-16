18dda99310000c72c951b16b533fef5d4a384b6d
oberstal <joberstaller@gmail.com>
2021-11-16 14:43:26 -0500

    update main doc

--- R/pfGO-package.R
+++ R/pfGO-package.R
@@ -1,6 +1,10 @@
+#' @details
+#' See the documentation for the [run.topGO.meta()] function for examples and common use-cases. See the "index" link at the bottom of this window for all included datasets and functions.
+#'
+#' @seealso [run.topGO.meta()]
 #' @keywords internal
 "_PACKAGE"
-#' @seealso [run.topGO.meta()]
+
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
