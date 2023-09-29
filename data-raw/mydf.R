d4e6cf9fa254ff7956be9266440ce43b351b2aa2
oberstal <joberstaller@gmail.com>
2023-09-28 20:58:08 -0400

    removed tracking for some dev files

--- data-raw/mydf.R
+++
@@ -1,7 +0,0 @@
-## code to prepare `mydf` dataset goes here
-#library(read.table)
-
-# example *piggyBac* mutant classification-data from pooled 1k heat shock screen (to use as "mydf" parameter for run.topGO.meta())
-input <- "data-raw/run.topGO.input.df.txt"
-exampleMydf <- read.delim(input, header = TRUE)
-usethis::use_data(exampleMydf, overwrite = TRUE)
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(pfGO)
-
-test_check("pfGO")
