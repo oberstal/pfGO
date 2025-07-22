98430f91992e662ad111fc74fad6e2f6a5ca5bc9
Jenna Oberstaller <joberstaller@gmail.com>
2025-07-22 13:31:04 -0400

    add make_enrichRes function

---
+++ R/make_enrichRes.R
@@ -0,0 +1,48 @@
+# 2025/03/03
+# Author: J. Oberstaller
+
+## make_enrichRes----
+#' Title
+#' @title make_enrichRes
+#' @description
+#' function to turn output of reformat_sigGenes into an enrichResult object, which can then be visualized in many different ways with functions from the enrichplot package.
+#'
+#' @param my.cpInput  a reformatted df from pfGO output all.combined.sig.genes.per.sig.term.tsv (using the reformat_sigGenes function)
+#' @param interestCategory  character string of your category of interest (should match a value from the interest.category column of my.cpInput)
+#' @param ontology  one of "BP", "CC" or "MF"
+#' @param pval_cutoff significance cutoff. Default is 0.05.
+#'
+#' @returns an enrichResult object (which can be visualized in various ways using functions from the enrichplot package). The enrichResult object class is described in the DOSE package.
+#' @export
+#'
+make_enrichRes <- function(my.cpInput, interestCategory, ontology = "BP", pval_cutoff = 0.05){
+
+  #require(DOSE)
+  #require(enrichplot)
+
+  res_df = my.cpInput[my.cpInput$interest.category==interestCategory,]
+
+  # make vector out of geneIDs (actually geneNames) for gene universe
+  geneNames = unlist(stringr::str_split(res_df$geneID, pattern = "/"), use.names = FALSE)
+
+  ## create enrichResult object (enrichResult object is a class defined from 'DOSE' package)
+  enrichRes = methods::new("enrichResult",
+                           readable = TRUE,
+                           result = res_df,
+                           pvalueCutoff = pval_cutoff,
+                           qvalueCutoff = 1,
+                           pAdjustMethod = "none",
+                           organism = "P.falciparum",
+                           ontology = ontology,
+                           keytype = "UNKNOWN",
+                           gene = res_df$geneID,
+                           universe = unique(geneNames[my.cpInput$interest.category!=interestCategory])
+  )
+
+  # fill in term similarity slot in enrichRes object (required for cnetplots)
+  termSim = enrichplot::pairwise_termsim(enrichRes)
+
+  # return the termSim enrichRes object
+  termSim
+
+}
